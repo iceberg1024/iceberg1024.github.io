2f2f2913ee7e2055c8344d500ecc89e4bccafcb6
iceberg1024 <iceberg1024@users.noreply.github.com>
2017-10-18 01:40:09 +1100

    edit 18.10.2017

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
+window.onload = function(){
 console.log("hello world");
 var myName = "张雪松";
 console.log(myName);
 alert(myName);
 var Name = document.querySelector('.name');
-Name.onclick = function(){
-    Name.innerHTML=myName;
+Name.onclick = function namechange(){
+    var text = this.innerHTML;
+    if(this.innerHTML ==="Xuesong Zhang"){
+    this.innerHTML="张雪松";
+}   else{
+    this.innerHTML="Xuesong Zhang";
+}
+}
+var showlink = document.querySelector('.img-wrapper');
+var git = document.querySelector('.github');
+    showlink.onmouseover=function show(){
+        git.style.display= inline;
+    }
+    showlink.onmouseout=function hide(){
+        git.style.display= hide;
+    }
 }
